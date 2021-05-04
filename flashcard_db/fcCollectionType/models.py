189d4f090b311ef8a7bf0dfe9e88da35ea11a0f8
cashmy <cmyers880@gmail.com>
2021-05-03 21:20:35 -0500

    Added directory configuration for media files (Flash Card addons)

--- flashcard_db/fcCollectionType/models.py
+++ flashcard_db/fcCollectionType/models.py
@@ -6,6 +6,9 @@ class FcCollectionType(models.Model):
     fcCollectionType_id = models.CharField(max_length=2, primary_key=True, unique=True)
     fcCollectionType_name = models.CharField(max_length=50, null=False, blank=False)
     fcCollectionType_desc = models.CharField(max_length=250, null=False, blank=True)
+    # fcCollectionType_audioFileUrl = models.FileField(max_length=250, null=True, blank=True)
+    # fcCollectionType_videoFileUrl = models.FileField(max_length=250, null=True, blank=True)
+    # fcCollectionType_imageFileUrl = models.ImageField(max_length=250, null=True, blank=True)
 
     def __str__(self):
         return self.fcCollectionType_name
