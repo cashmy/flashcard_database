53c3eacfa6dedbde97a6dd817e026569f6258288
cashmy <cmyers880@gmail.com>
2021-05-09 21:57:56 -0500

    Renamed collection id FK field in flashcard

--- flashcard_db/fcCollections/models.py
+++ flashcard_db/fcCollections/models.py
@@ -6,6 +6,7 @@ class FcCollection(models.Model):
     fcCollection_id = models.AutoField(primary_key=True, unique=True)
     fcCollection_title = models.CharField(max_length=50, null=False, blank=True)
     fcCollectionType_id = models.ForeignKey('fcCollectionType.FcCollectionType', on_delete=models.CASCADE)
+    fcCollection_image = models.ImageField(upload_to='uploads/', blank=True)
 
     def __str__(self):
         return self.fcCollection_title
--- flashcard_db/flashCard/models.py
+++ flashcard_db/flashCard/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 class FlashCard(models.Model):
-    fcCollection_id = models.ForeignKey('fcCollections.FcCollection', on_delete=models.CASCADE)
+    fcCollection = models.ForeignKey('fcCollections.FcCollection', on_delete=models.CASCADE)
     face = models.CharField(max_length=250, null=False, blank=True)
     back = models.CharField(max_length=250, null=False, blank=True)
 
